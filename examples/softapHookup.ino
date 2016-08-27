5cd761fe10abf5b03c763435ecda4e4cb397ca00
Chris Parent <chris@motorcitysoftware.com>
2016-08-26 20:19:36 -0400

    CP - moved files to folders

--- examples/softapHookup.ino
+++ examples/softapHookup.ino
@@ -1,8 +1,7 @@
-#include "softapHookup.h"
+#include "SoftapHookup.h"
 #include <ESP8266WebServer.h>
 
 ESP8266WebServer server(80);
-//#define BUTTON_PIN 6
 SoftapHookup softaphookup("myespssid", "password", &server);
 
 void setup() {
